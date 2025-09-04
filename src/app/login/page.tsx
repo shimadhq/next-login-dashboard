bd7a4ea4a347c25fa584801990eec41dfc0d926f
shimadhq <dehghanishima78@gmail.com>
2025-09-04 15:47:39 +0330

    🎨 styling login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,26 @@
 "use client";
 import React, { useState } from "react";
+import LoginBox from "@/Components/Login/LoginBox";
 import Image from "next/image";
 
 function LoginPage () {
     return (
-        <div className="fles items-center justify-center bg-[#8CA6FF] xl:p-24">
-            <div className="flex items-center justify-between bg-white rounded-xl border border-[#A5A5A5]">
-                <div className="flex flex-col items-center justify-center w-[60%]">
-
-                </div>
+        <div className="flex w-full h-full items-center justify-between bg-white rounded-xl border border-[#A5A5A5]">
+            <div className="xl:w-[58%]">
+                <LoginBox />
+            </div>
+            <div className="xl:w-[42%]">
+                <Image
+                    width={1000}
+                    height={1000}
+                    src="/Images/login-image.png"
+                    className="w-full"
+                    alt="login-image"
+                    priority
+                />
             </div>
         </div>
     );
-};
+}
 
 export default LoginPage;
